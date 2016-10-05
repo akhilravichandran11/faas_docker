cf1cceba03ea0faa8e60356cce4bcb4b45cc63ee
Akhil BYB <aravic12@asu.edu>
2016-10-05 01:03:19 -0700

    mc

--- flask_app/app/main.py
+++ flask_app/app/main.py
@@ -5,24 +5,42 @@ import custom_util
 
 app = Flask(__name__)
 
-client = docker.Client(base_url='unix://var/run/docker.sock')
+docker_client = docker.Client(base_url='unix://var/run/docker.sock')
+
+db_manager_url = "http://192.168.1.9:8080"
+
+db_manager_url_api = {
+    "request":{
+    "check_status": "dbmanager/rest/request"
+    }
+}
 
 @app.route("/")
 def hello():
     return  "Welcome To Madhatterz - FAAS"
+
+
+@app.route("/request/check_status/<string:request_id>" , methods = ['GET'] )
+def request_check_status():
+
+
     
 
 @app.route("/containers/list")    
 def containers():
-    containerz = client.containers()
-    return  custom_util.custom_print(containerz,"<br>")
-    
-    
-@app.route('/containers/logs/<variable>' , methods = ['GET'])
+    log_result = ""
+    try:
+        containerz = docker_client.containers()
+        log_result = custom_util.custom_print(containerz,"<br>") 
+    except Exception as e:
+        log_result = str(e)
+    return  log_result
+
+@app.route('/containers/logs/<variable>' , methods = ['GET'] )
 def container_logs(variable):
     log_result = ""
     try:
-        log_result = client.logs(variable).replace("\n","<br>")
+        log_result = docker_client.logs(variable).replace("\n","<br>")
     except Exception as e:
         log_result =  str(e)
     return log_result
