471f4ccd233fc2df9a888be15f0e8d9998e53595
Akhil <akhil.ravichandran@asu.edu>
2016-10-04 19:13:42 -0700

    mc

--- misc/requests_to_db_manager.py
+++ misc/requests_to_db_manager.py
@@ -1,8 +1,14 @@
-import json
+import sys
+import os
 import requests
+import json
+
 data = {'temperature':'24.3'}
 data_json = json.dumps(data)
 payload = {'json_payload': data_json, 'apikey': 'YOUR_API_KEY_HERE'}
 r = requests.get('http://192.168.1.9:8080/dbmanager/rest/users')
 
-print r
+print r.status_code
+print r.json()
+print r.text
+print r.headers['content-type']
