f358fbe1c7041ec4567fabffa2014ae3b40ad32b
= <=>
2016-10-04 18:41:20 -0700

    mc

--- misc/requests_to_db_manager.py
+++ misc/requests_to_db_manager.py
@@ -3,4 +3,6 @@ import requests
 data = {'temperature':'24.3'}
 data_json = json.dumps(data)
 payload = {'json_payload': data_json, 'apikey': 'YOUR_API_KEY_HERE'}
-r = requests.get('http://myserver/emoncms2/api/post', data=payload)
+r = requests.get('http://192.168.1.9:8080/dbmanager/rest/users')
+
+print r
